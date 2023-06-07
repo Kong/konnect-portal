cd8b8c60cdf85a5abfd444161bbeca763b9ca19c
Andrew Wylde <andrew.wylde@konghq.com>
2023-06-07 11:38:50 -0500

    fix(ci): commitlint checks should ignore dependabot (#78)

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
+  ignores: [(message) => /^Bumps \[.+]\(.+\) from .+ to .+\.$/m.test(message)], // from https://github.com/dependabot/dependabot-core/issues/2445#issuecomment-949633412
   rules: {
     'header-max-length': [2, 'always', 108]
   }
