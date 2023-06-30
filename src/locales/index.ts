8cc43dd8ed3527f26e6d8f5ea49ed39044d57637
svenwal <sven@svenwal.de>
2023-06-30 10:19:53 +0200

    ci: german translation,type:build

--- src/locales/index.ts
+++ src/locales/index.ts
@@ -1,7 +1,8 @@
 import { en } from './en'
+import { de } from './de'
 
 // If you wish to define additional locales, add them here separated by `|`
-export type DefinedLocales = 'en'
+export type DefinedLocales = 'en|de'
 
 export const locales: Record<DefinedLocales, typeof en> = {
   en
