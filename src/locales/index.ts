a902506ab66156e3ccb873f33ed14aeeb3b7cfbe
battlebyte <jordi@fernandez.name>
2023-07-05 16:54:07 +0200

    Spanish translation

--- src/locales/index.ts
+++ src/locales/index.ts
@@ -1,8 +1,10 @@
 import { en } from './en'
+import { es_ES } from './es_ES'
 
 // If you wish to define additional locales, add them here separated by `|`
-export type DefinedLocales = 'en'
+export type DefinedLocales = 'en | es_ES'
 
 export const locales: Record<DefinedLocales, typeof en> = {
-  en
+  en,
+  es_ES,
 }
