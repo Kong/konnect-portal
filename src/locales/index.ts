c4471de861b7cef22b1f1af9b59b2645384a87c0
battlebyte <jordi@fernandez.name>
2023-07-06 09:40:29 +0200

    Catalan translation.

--- src/locales/index.ts
+++ src/locales/index.ts
@@ -1,8 +1,10 @@
 import { en } from './en'
+import { ca_ES } from './ca_ES'
 
 // If you wish to define additional locales, add them here separated by `|`
-export type DefinedLocales = 'en'
+export type DefinedLocales = 'en | ca_ES'
 
 export const locales: Record<DefinedLocales, typeof en> = {
-  en
+  en,
+  ca_ES
 }
