f08b4f5a3e7e26be5fec9f39d291f2911db8f2f0
battlebyte <jordi@fernandez.name>
2023-07-06 17:23:21 +0200

    Update src/locales/index.ts
    Co-authored-by: Mike Swierenga <mike.swierenga@gmail.com>

--- src/locales/index.ts
+++ src/locales/index.ts
@@ -2,7 +2,7 @@ import { en } from './en'
 import { ca_ES } from './ca_ES'
 
 // If you wish to define additional locales, add them here separated by `|`
-export type DefinedLocales = 'en | ca_ES'
+export type DefinedLocales = 'en' | 'ca_ES'
 
 export const locales: Record<DefinedLocales, typeof en> = {
   en,
