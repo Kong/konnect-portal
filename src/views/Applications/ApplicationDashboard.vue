3f38b5e2543a69175c406c5a24ad12d47aa45468
mihai-peteu <mihai.peteu@konghq.com>
2023-09-08 11:01:29 -0700

    chore(analytics): remove feature flag

--- src/views/Applications/ApplicationDashboard.vue
+++ src/views/Applications/ApplicationDashboard.vue
@@ -17,7 +17,7 @@
         class="mb-5"
         :title="helpText.analytics.dashboard"
       />
-      <div v-if="contextualAnalytics && hasProductVersions">
+      <div v-if="hasProductVersions">
         <div
           class="analytics-filters d-flex flex-grow-1 justify-content-between align-items-baseline mb-6"
         >
@@ -112,8 +112,6 @@ import getMessageFromError from '@/helpers/getMessageFromError'
 import usePortalApi from '@/hooks/usePortalApi'
 import useToaster from '@/composables/useToaster'
 import useAllowedTimeframes from '@/composables/useAllowedTimeframes'
-import { FeatureFlags } from '@/constants/feature-flags'
-import useLDFeatureFlag from '@/hooks/useLDFeatureFlag'
 import {
   datePickerSelectionToTimeframe,
   Timeframe,
@@ -130,9 +128,6 @@ import PageTitle from '@/components/PageTitle.vue'
 import { useAppStore, useI18nStore } from '@/stores'
 import cloneDeep from 'lodash.clonedeep'
 import { PortalTimeframeKeys } from '@/types/vitals'
-
-// @ts-ignore
-const contextualAnalytics = useLDFeatureFlag(FeatureFlags.PortalContextualAnalytics, false)
 
 const { notify } = useToaster()
 const errorMessage = ref('')
