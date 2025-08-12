6f3469e1fff779f885bba494709828aeba5a520f
Nathan Bailey <nbailey0001@gmail.com>
2025-08-12 15:41:31 -0700

    Revert "fix(spec-renderer): cache bust spec loading behind FF [TDX-6432] (#594)"
    This reverts commit 6e88ae582efc3400790a55bbbd531c7f2a61ada0.

--- src/constants/feature-flags.ts
+++ src/constants/feature-flags.ts
@@ -1,5 +1,4 @@
 export enum FeatureFlags {
   newMarkdownRender = 'tdx-4186-new-markdown',
-  publicLabelsUI = 'tdx-4824-public-labels-ui',
-  cacheBustSpecLinks = 'tdx-6432-cache-burst-spec-links'
+  publicLabelsUI = 'tdx-4824-public-labels-ui'
 }
